e86d439f0ff50fe985d67da3ee00a929bc5b0b5c
Sven Werner <svenwerner1997@gmail.com>
2022-07-31 15:28:51 +0200

    #1 added main app layout

--- src/web-app/ClientApp/src/App.tsx
+++ src/web-app/ClientApp/src/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { Page1 } from "./Pages/Page1";
-import { Page2 } from "./Pages/Page2";
-
+import { AppLayout } from "./Components/Layout/AppLayout";
 
 export const App = () => {
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/page1" element={<Page1 />} />
-                <Route path="/page2" element={<Page2 />} />
-            </Routes>           
-        </BrowserRouter>
+        <AppLayout>
+            <p>Test</p>
+        </AppLayout>
     );
 }
